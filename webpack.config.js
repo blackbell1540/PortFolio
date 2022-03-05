1922efe775489973c39043112dce86fa858cda5b
BlackBell <mupasa4807@gmail.com>
2022-03-05 18:44:03 +0900

    Add todolist page

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,14 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: {
+        main: './src/index.js',
+        'todolist/main': './src/todolist/index.js'
+    },
     output: {
-        filename: 'main.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true
     },
     devServer: {
         hot: true,
@@ -19,7 +23,11 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './src/index.html',
             filename: 'index.html'
-        })
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/todolist/index.html',
+            filename: '/todolist/index.html'
+        }),
     ],
     module: {
         rules: [
